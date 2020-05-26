0f3803c4121a9af8bccff9d5e97e09ea522bbec1
Raymond R. Balise <balise@miami.edu>
2020-05-26 03:08:11 -0400

    The lie is number 2

---
+++ aboutZainab.R
@@ -0,0 +1,9 @@
+#' @export aboutRay
+aboutZainab <- function() {
+  cat("Which of these is a lie: \n")
+  cat("1. I hvae a cute kid\n")
+  cat("2. I have visited Disney World more than 5 times\n")
+  cat("3. I worked with the ministry of health in Saudi Arabia for two years \n")
+  cat("4. I am from Saudi Arabia,but my mother is not from the same country\n")
+  cat("5. I like cooking and drwaing\n")
+}
